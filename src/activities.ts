ebe47bf116ae983d887aa36a831d22f9aa1f94ea
Bob Reselman <bob@CogArtTech.com>
2023-03-13 09:51:13 -0700

    adding pause

--- src/activities.ts
+++ src/activities.ts
@@ -1,5 +1,9 @@
 import {ApplicationFailure} from "@temporalio/workflow";
 
+
+//  milliseconds * seconds * minutes * hours = .5 hours
+const PAUSE_LENGTH= 1000 * 60 * 60 * .5;
+
 export async function wakeUp(wakeUpTime: string): Promise<string> {
     const msg = 'I got up at : ' + wakeUpTime + '.';
     console.log(msg);
@@ -9,6 +13,7 @@ export async function wakeUp(wakeUpTime: string): Promise<string> {
 export async function takeShower(): Promise<string> {
     const msg = 'I am taking a shower!';
     console.log(msg);
+    await pause(PAUSE_LENGTH);
     return msg;
 }
 
@@ -16,11 +21,18 @@ export async function makeBreakfast(): Promise<string> {
     const tasks = ['brew coffee', 'make toast', 'fry egg']
     tasks.forEach(task => {console.log(task)})
     const msg =  'Here is what I did: ' + tasks.join(" ")
+    await pause(PAUSE_LENGTH);
     return msg;
 }
 
 export async function brushTeeth(): Promise<string> {
     const msg = 'I brushed my teeth'
     console.log(msg);
+    await pause(PAUSE_LENGTH);
     return msg;
 }
+
+async function pause(milliseconds: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
